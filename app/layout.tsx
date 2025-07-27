9baa10dd8cbd2d61647a93ddfcda6a24fa0c96ef
louisefor <louise@saveabl.se>
2025-07-27 13:47:37 +0200

    Use manual head.tsx for correct OG image rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,6 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 import { dmSerif, poppins } from "./fonts";
 
-export const metadata = {
-  title: "Portfolio – Louise Forsby",
-  description: "Strategic marketer with hands-on UX/UI experience",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
 export default function RootLayout({
   children,
 }: {
